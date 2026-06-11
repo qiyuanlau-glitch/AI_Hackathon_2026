ea5eb748869322e6ff06affd4e79bf32654931de
Choo Jun Yu <junyu.choo@simpletruss.com>
2026-06-11 18:57:15 +0800

    feat: add guideline knowledge base (Article C gardening policy)

---
+++ guideline-kb.js
@@ -0,0 +1,28 @@
+(function(){
+  window.GUIDELINE_KB = {
+    policyName: 'Elmwood Heights Community Policy',
+    articles: [
+      {
+        id: 'C',
+        title: 'Gardening',
+        clauses: [
+          {
+            id: 'C1',
+            title: 'Trimming of trees',
+            text: 'Trees may only be trimmed into the shape of a circle.',
+          },
+          {
+            id: 'C2',
+            title: 'Cutting down trees',
+            text: 'Only trees deemed dying or browning by a panel gardener may be cut down.',
+          },
+          {
+            id: 'C3',
+            title: 'Planting flowers',
+            text: 'Residents may not plant flowers that were not originally there without prior approval.',
+          },
+        ],
+      },
+    ],
+  };
+})();
